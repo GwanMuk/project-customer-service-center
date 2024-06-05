51451993b219a93d375c559b2281d2b6024f7935
​GwanMuk <mkm1022@naver.com>
2024-06-05 10:39:27 +0900

    first commit

---
+++ src/main/java/com/unknown/controller/NavController.java
@@ -0,0 +1,24 @@
+package com.unknown.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController {
+
+    @GetMapping("/expressDelivery")
+    public String expressDelivery() {
+        return "expressDelivery";
+    }
+    
+    @GetMapping("/orangeMembers")
+    public String orangeMembers() {
+        return "orangeMembers";
+    }
+    
+    @GetMapping("/event")
+    public String event() {
+        return "event";
+    }
+  
+}
